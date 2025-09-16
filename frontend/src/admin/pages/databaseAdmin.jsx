78a018218aaf903314a3419ffc620d20dbada010
atmwijaya <arrasyidaw3@gmail.com>
2025-09-17 00:00:04 +0700

    update fakultas

--- frontend/src/admin/pages/databaseAdmin.jsx
+++ frontend/src/admin/pages/databaseAdmin.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 // Data fakultas dan jurusan
 const fakultasJurusan = {
   Hukum: ["Hukum"],
-  Ekonomi: [
+  "Ekonomika dan Bisnis": [
     "Akuntansi",
     "Ilmu Ekonomi",
     "Manajemen",
